0f2f959d0dec6d65f5be17e6d57182e7cb3f4bc0
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-08-08 14:20:35 +0900

    Update Camera Counting testing.py

--- Camera Counting testing.py
+++ Camera Counting testing.py
@@ -2,16 +2,15 @@ import cv2
 import numpy as np
 from time import sleep
 
-largura_min=130 # 최소 직사각형 너비
-altura_min=130 # 직사각형의 최소 높이
+largura_min=100 # 최소 직사각형 너비
+altura_min=100 # 직사각형의 최소 높이
 
-offset=6 # 픽셀 간 허용 오차
+offset=10 # 픽셀 간 허용 오차
 
-pos_linha=300 # 카운트 라인 위치
+pos_linha=150 # 카운트 라인 위치
+linw = 10 # 카운트 라인 두께
 
-delay= 60 # 비디오 fps
-
-line_d = 20
+delay= 144 # 비디오 fps
 
 detec = []
 carros= 0
@@ -27,6 +26,11 @@ def pega_centro(x, y, w, h):
 cap = cv2.VideoCapture(0) # 웹캠 열기
 subtracao = cv2.bgsegm.createBackgroundSubtractorMOG() # 배경 제거
 
+# 원본 동영상 크기 정보
+w = cap.get(cv2.CAP_PROP_FRAME_WIDTH) # 640
+h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # 480
+print("원본 동영상 너비(가로) : {}, 높이(세로) : {}".format(w, h))
+
 while True:
     ret , frame1 = cap.read() # 웹캠 읽어오기
     tempo = float(1/delay) # 프레임 설정
@@ -40,7 +44,7 @@ while True:
     dilatada = cv2.morphologyEx (dilatada, cv2. MORPH_CLOSE , kernel)
     contorno, h = cv2.findContours(dilatada,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) # 외곽선 검출 함수, (입력 영상, 외곽선 검출 모드, 외곽선 근사화 방법, 검출된 외곽선 좌표, 외곽선 계층 정보, 좌표 값 이동 옵셋)
     
-    cv2.line(frame1, (0, pos_linha), (1200, pos_linha), (255,127,0), line_d)  # 카운트 라인 그리기 (이미지 파일, 시작점 좌표 (x, y), 종료점 좌표 (x, y), 색상 (blue, green, red), 선 두께, 선 종류, fractional bit)
+    cv2.line(frame1, (pos_linha, 0), (pos_linha, 480), (255,127,0), linw)  # 카운트 라인 그리기 (이미지 파일, 시작점 좌표 (x, y), 종료점 좌표 (x, y), 색상 (blue, green, red), 선 두께, 선 종류, fractional bit)
     for(i,c) in enumerate(contorno): # 카운트 라인에 닿았을 때
         (x,y,w,h) = cv2.boundingRect(c) # 주어진 점을 감싸는 최소 크기 사각형(바운딩 박스)를 반환한다.
         validar_contorno = (w >= largura_min) and (h >= altura_min) # 직사각형 최소 높이, 너비와 비교
@@ -50,20 +54,22 @@ while True:
         cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2) # 사각형 그리기, (이미지 파일, 시작점 좌표 (x, y), 종료점 좌표 (x, y), 색상 (blue, green, red), 선 두께, 선 종류, fractional bit)
         centro = pega_centro(x, y, w, h)
         detec.append(centro)
-        cv2.circle(frame1, centro, 4, (0, 0,255), -1) # 원 그리기
+        cv2.circle(frame1, centro, 4, (0, 0,255), -1) # 원 그리기a
 
         for (x,y) in detec:
-            if y<(pos_linha+offset) and y>(pos_linha-offset): # 카운트 라인 안에 들어올 때
+            if x<(pos_linha+offset) and x>(pos_linha-offset): # 카운트 라인 안에 들어올 때
                 carros+=1 # 카운트 값 증가
-                cv2.line(frame1, (0, pos_linha), (1200, pos_linha), (0,127,255), line_d) # 선 색상 변경
+                cv2.line(frame1, (pos_linha, 0), (pos_linha, 480), (0,127,255), linw) # 선 색상 변경
                 detec.remove((x,y)) # 초기화
-                print("car is detected : "+str(carros)) # 카운트 출력
+                print("Person is detected : "+str(carros)) # 카운트 출력
        
-    cv2.putText(frame1, "Person Count : "+str(carros), (0, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255),3) # 텍스트 출력, (이미지 파일, 출력 문자, 출력 문자 시작 위치 좌표 (좌측 하단), fontFace,  폰트 크기, 폰트 색상, 폰트 두께, 선 종류, org 사용 옵션 (True: 좌측 하단, False: 좌측 상단))
+    cv2.putText(frame1, "Person COUNT : "+str(carros), (0, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2) # 텍스트 출력, (이미지 파일, 출력 문자, 출력 문자 시작 위치 좌표 (좌측 하단), fontFace,  폰트 크기, 폰트 색상, 폰트 두께, 선 종류, org 사용 옵션 (True: 좌측 하단, False: 좌측 상단))
     cv2.imshow("Video Original" , frame1) # 카메라 영상 출력
     cv2.imshow("Detectar",dilatada) # 회색 카메라 영상 출력
 
-    if cv2.waitKey(1) == 27:
+    if cv2.waitKey(33) == ord('a'): # a 누르면 카운트 초기화
+      carros = 0
+    if cv2.waitKey(1) == 27: # esc 누르면 프로그램 종료
         break
     
 cv2.destroyAllWindows()
