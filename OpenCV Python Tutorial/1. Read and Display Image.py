3ffa700a525423036082df97dec845f24e81ac5f
AI-WonYJ <101448204+AI-WonYJ@users.noreply.github.com>
2022-08-31 21:38:49 +0900

    Delete OpenCV Python Tutorial directory

--- OpenCV Python Tutorial/1. Read and Display Image.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-img = cv2.imread('images\Seoul_NTower.png') # 이미지 불러오기
-cv2.imshow('sample image',img) # 이미지 출력하기
-cv2.waitKey(0) # 키가 눌릴 때까지 기다리기
-cv2.destroyAllWindows() # 키가 눌리면 이미지 지우기
--- OpenCV Python Tutorial/6. Edge Detection.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img = cv2.imread('images\Seoul_NTower.png')
-edges = cv2.Canny(img, 100, 200) # 이미지에서 초점이 맞춰진 개체의 가장자리 찾는 구문
-
-cv2.imshow("Edge Detected Image", edges)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
